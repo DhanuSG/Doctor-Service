cf4775eb637ffc5a113fb56cc7a9a8abeacb5b41
Dhanushka Ganhewage <50699988+DhanuSG@users.noreply.github.com>
2020-05-04 12:25:05 +0530

    set NIC validation

--- Doctor Service/WebContent/Components/doctor.js
+++ Doctor Service/WebContent/Components/doctor.js
@@ -1,36 +1,40 @@
 //Hide alerts-Register
 $(document).ready(function()
 {
-$("#alertSuccess").hide();
-$("#alertError").hide();
-}); 
- 
-//Hide alerts-Email
-$(document).ready(function()
-{
-$("#alertErrorEmail").hide();
-}); 
-
-//Form validation
-var status = validateForm();
-// If not valid
-if (status != true)
-{
- $("#alertError").text(status);
- $("#alertError").show();
-return;
-}
-
-//Display success message
-$("#alertSuccess").text("Registered successfully.");
-$("#alertSuccess").show();
-
-$("#doctorRegister")[0].reset();
-
-//Validate form elements
-function validateForm(){
+//hide area of error messages
+	$("#alertErrorNic").hide();
+	$("#alertErrorFirstName").hide();
+	$("#alertErrorLastName").hide();
+	$("#alertErrorContactNumber").hide();
+	$("#alertErrorGender").hide();
+	$("#alertErrorCategory").hide();
+	$("#alertErrorEmail").hide();
+	$("#alertErrorPassword").hide();
 	
-	//email
+//check the number of characters in NIC
+	$("#nic").focusout(function(){
+		checkNic();
+	});
+
+//check email is valid or not
+	$("#email").focusout(function(){
+		checkEmail();
+	});
+
+//NIC validation
+function checkNic(){
+	let nicLength = $("#nic").val().length();
+	
+	if (nicLength == 10 || nicLength == 12){
+		$("#alertErrorNic").hide();
+	}
+	else{
+		$("#alertErrorNic").text("NIC should be 10 or 12 characters.");
+		$("#alertErrorNic").show();
+	}
+}
+//Email validation
+function checkEmail(){
 		let email = $("#email").val().trim();
 	  	let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	  
@@ -41,9 +45,8 @@ function validateForm(){
 	  		$("#alertErrorEmail").text("Please enter a valid email");
 	  		$("#alertErrorEmail").show();
 	  	}
-
-	  	
-};
+}
+});
 
 
 
